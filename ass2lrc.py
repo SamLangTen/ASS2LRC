7783970140e00505a98ee90ec3fd6b3fa715de84
SamLangTen <samlangten@outlook.com>
2018-09-16 00:12:35 +0800

    add opt

--- ass2lrc.py
+++ ass2lrc.py
@@ -1,8 +1,41 @@
 from ass_interaction import *
+import getopt
+
+
+def display_usage(name):
+    print("usage: python " + name + " [--help] -i <inputfile> -o <outputfile> [-t mileseconds]")
+    print("")
+    print("-i --input\tAn ASS filename which will be converted to lrc.")
+    print("-o --output\tA LRC filename where the convered text will be stored.")
+    print("-t --split_timespan\tA new blank line will be added if the start time of two between dialogues greater than this argument, default is 1000ms")
 
 if __name__ == "__main__":
-    #filename = sys.argv[1]
-    filename = "test.ass"
-    dialogues = ass_dialogue.load_lrc(filename)
-    print(convert_to_normal_lrc(dialogues, 10))
+    inputfilename = ""
+    outputfilename = ""
+    split_timespan = 1000
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:", ["help", "input=", "output=", "split_timespan="])
+    except getopt.GetoptError:
+        display_usage(sys.argv[0])
+        sys.exit(2)
+    for opt, arg in opts:
+        if (opt in ("-i", "--input")):
+            inputfilename = arg
+        elif (opt in ("-o", "--output")):
+            outputfilename = arg
+        elif (opt in ("-t", "--split_timespan")):
+            split_timespan = int(arg)
+        elif (opt in ("-h", "--help")):
+            display_usage(sys.argv[0])
+            sys.exit(2)
+
+    if inputfilename == "" or outputfilename == "":
+        display_usage(sys.argv[0])
+        sys.exit(2)
+    #convert
+    dialogues = ass_dialogue.load_lrc(inputfilename)
+    output_text = convert_to_normal_lrc(dialogues, split_timespan)
+    fout = open(outputfilename, "w")
+    fout.write(output_text)
+    fout.close()
     
