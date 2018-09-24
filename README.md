2d0cf91e623209efdb8feef2b87762d7e9dd643b
SamLangTen <samlangten@outlook.com>
2018-09-25 00:05:49 +0800

    add style filter

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # ASS2LRC
 This script can convert ASS subtitle file to common LRC lryics file.
 
-    usage: python .\ass2lrc.py [--help] -i <inputfile> -o <outputfile> [-t mileseconds]
+    usage: python .\ass2lrc.py [--help] -i <inputfile> -o <outputfile> [-t mileseconds] [-s stylename]
     
-    -i --input      An ASS filename which will be converted to lrc.
-    -o --output     A LRC filename where the convered text will be stored.
+    -i --input              An ASS filename which will be converted to lrc
+    -o --output             A LRC filename where the convered text will be stored
     -t --split_timespan     A new blank line will be added if the start time of two between dialogues greater than this argument, default is 1000ms
+    -s --convert_styles     A filter to decide which style of dialogue in ASS should be converted into LRC file
 
 Since LRC file does not support ending timestamp, some lryics followed with a long period time of music will be "frozen". This script can add a new blank lyrics whose start timestamp is ending timestamp of the former lyrics. If timespan between the former ending time and the latter start time is greater than option split_timespan, a new blank line will be added.
+
+Some ASS files may contain more than one kind of style because different style represents different kind of subtitle. Convert Styles setting can filter the dialogues with style which you want to convert into LRC dialogues.
--- src/ass2lrc.py
+++ src/ass2lrc.py
@@ -3,18 +3,20 @@ import getopt
 
 
 def display_usage(name):
-    print("usage: python " + name + " [--help] -i <inputfile> -o <outputfile> [-t mileseconds]")
+    print("usage: python " + name + " [--help] -i <inputfile> -o <outputfile> [-t mileseconds] [-s stylename]")
     print("")
-    print("-i --input\tAn ASS filename which will be converted to lrc.")
-    print("-o --output\tA LRC filename where the convered text will be stored.")
+    print("-i --input\tAn ASS filename which will be converted to lrc")
+    print("-o --output\tA LRC filename where the convered text will be stored")
     print("-t --split_timespan\tA new blank line will be added if the start time of two between dialogues greater than this argument, default is 1000ms")
+    print("-s --convert_styles\tA filter to decide which style of dialogue in ASS should be converted into LRC file")
 
 if __name__ == "__main__":
     inputfilename = ""
     outputfilename = ""
     split_timespan = 1000
+    convert_styles = list()
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:", ["help", "input=", "output=", "split_timespan="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:s:", ["help", "input=", "output=", "split_timespan=","convert_styles="])
     except getopt.GetoptError:
         display_usage(sys.argv[0])
         sys.exit(2)
@@ -25,6 +27,8 @@ if __name__ == "__main__":
             outputfilename = arg
         elif (opt in ("-t", "--split_timespan")):
             split_timespan = int(arg)
+        elif (opt in ("-s", "--convert_styles")):
+            convert_styles.append(arg)
         elif (opt in ("-h", "--help")):
             display_usage(sys.argv[0])
             sys.exit(2)
@@ -34,6 +38,13 @@ if __name__ == "__main__":
         sys.exit(2)
     #convert
     dialogues = ass_dialogue.load_lrc(inputfilename)
+    #if convert styles set
+    if len(convert_styles) != 0:
+        new_dialogues = list()
+        for style_item in dialogues:
+            if style_item.style in convert_styles:
+                new_dialogues.append(style_item)
+        dialogues = new_dialogues
     output_text = convert_to_normal_lrc(dialogues, split_timespan)
     fout = open(outputfilename, "w")
     fout.write(output_text)
