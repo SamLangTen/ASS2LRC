69ccd3b6e319f05c0758271edea80975da4242b6
SamLangTen <samlangten@outlook.com>
2018-09-27 20:16:02 +0800

    add compact

--- src/ass2lrc.py
+++ src/ass2lrc.py
@@ -4,12 +4,13 @@ import getopt
 
 def display_usage(name):
     print("usage: python " + name +
-          " [--help] -i <inputfile> -o <outputfile> [-t mileseconds] [-s stylename]")
+          " [--help] -i <inputfile> -o <outputfile> [-t mileseconds] [-s stylename] [-c]")
     print("")
     print("-i --input\tAn ASS filename which will be converted to lrc")
     print("-o --output\tA LRC filename where the convered text will be stored")
     print("-t --split_timespan\tA new blank line will be added if the start time of two between dialogues greater than this argument, default is 1000ms")
     print("-s --convert_styles\tA filter to decide which style of dialogue in ASS should be converted into LRC file")
+    print("-c --compact\tCombine timestamp of dialogues with same text, not supported by all player")
 
 
 if __name__ == "__main__":
@@ -17,9 +18,10 @@ if __name__ == "__main__":
     outputfilename = ""
     split_timespan = 1000
     convert_styles = list()
+    is_compact = False
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:s:", [
-                                   "help", "input=", "output=", "split_timespan=", "convert_styles="])
+        opts, args = getopt.getopt(sys.argv[1:], "hi:o:t:s:c", [
+                                   "help", "input=", "output=", "split_timespan=", "convert_styles=","compact"])
     except getopt.GetoptError:
         display_usage(sys.argv[0])
         sys.exit(2)
@@ -32,6 +34,8 @@ if __name__ == "__main__":
             split_timespan = int(arg)
         elif (opt in ("-s", "--convert_styles")):
             convert_styles.append(arg)
+        elif (opt in ("-c", "--compact")):
+            is_compact = True
         elif (opt in ("-h", "--help")):
             display_usage(sys.argv[0])
             sys.exit(2)
@@ -44,7 +48,10 @@ if __name__ == "__main__":
     # if convert styles set
     if len(convert_styles) != 0:
         dialogues = ass_styles_filter(dialogues, convert_styles)
-    output_text = convert_to_normal_lrc(dialogues, split_timespan)
+    if not is_compact:
+        output_text = convert_to_normal_lrc(dialogues, split_timespan)
+    else:
+        output_text = convert_to_compact_lrc(dialogues, split_timespan)
     fout = open(outputfilename, "w")
     fout.write(output_text)
     fout.close()
--- src/ass_interaction.py
+++ src/ass_interaction.py
@@ -10,6 +10,7 @@ class ass_dialogue_word(object):
         self.word = word
         self.start_time = start_time
 
+
 class ass_dialogue(object):
 
     @staticmethod
@@ -25,7 +26,7 @@ class ass_dialogue(object):
                     phrased[2].split(":")[2].split(".")[0]), int(phrased[2].split(":")[2].split(".")[1])*10000)
                 style = phrased[3]
                 words = list()
-                #extract effect code: (\{[^{]*\}){0,1}[^{]*
+                # extract effect code: (\{[^{]*\}){0,1}[^{]*
                 no_k_text = ""
                 last_text = ""
                 last_text = last_text.join(phrased[9:])
@@ -35,7 +36,7 @@ class ass_dialogue(object):
                         break
                     last_text = last_text.replace(first_re.group(), "")
                     item = first_re.group()
-                    #get K
+                    # get K
                     ec_match = re.match(r"\{\\[K|k]\d*\}", item)
                     if (ec_match is not None):
                         word = ass_dialogue_word(item.replace(re.match(r"\{\\[K|k]\d*\}", item).group(), ""), start_time + datetime.timedelta(
@@ -43,13 +44,13 @@ class ass_dialogue(object):
                         if not word.word == "":
                             words.append(word)
                     else:
-                        #clear effect code
+                        # clear effect code
                         ec2_match = re.match(r"\{[^\{|\}]*\}", item)
                         if ec2_match is not None:
                             no_k_text += item.replace(ec2_match.group(), "")
                         else:
                             no_k_text += item
-                #if no effect code
+                # if no effect code
                 if len(words) == 0:
                     words = no_k_text.strip()
                 dialogue = ass_dialogue(start_time, end_time, style, words)
@@ -57,7 +58,7 @@ class ass_dialogue(object):
         files.close()
         return dialogues
 
-    def __lt__(self, other):  
+    def __lt__(self, other):
         if self.start_time < other.start_time:
             return True
         return False
@@ -68,6 +69,7 @@ class ass_dialogue(object):
         self.style = style
         self.text = text
 
+
 def ass_styles_filter(dialogues, styles):
     new_dialogues = list()
     for style_item in dialogues:
@@ -75,13 +77,59 @@ def ass_styles_filter(dialogues, styles):
             new_dialogues.append(style_item)
     return new_dialogues
 
+
+def convert_to_compact_lrc(dialogues, space_end_timespan_ms):
+    generated_text = ""
+    dialogues.sort()
+    blanks_dialogues = list()
+    # Add blank line
+    for index, item in enumerate(dialogues):
+        if not index == len(dialogues) - 1:
+            td = dialogues[index + 1].start_time - item.end_time
+            if td.total_seconds() * 1000 > space_end_timespan_ms:
+                lryic = __generate_lrc_timestamp(item.start_time)
+                blanks_dialogues.append(lryic)
+    # Generate
+    i = 0
+    while len(dialogues) > 0:
+        this_line_generated_text = __generate_lrc_timestamp(dialogues[i].start_time)
+        j = i + 1
+        this_line_text = __convert_ass_effect_word_to_text(dialogues[i].text)
+        while j < len(dialogues):
+            if __convert_ass_effect_word_to_text(dialogues[j].text) == this_line_text:
+                this_line_generated_text += __generate_lrc_timestamp(
+                    dialogues[j].start_time)
+                dialogues.pop(j)
+            else:
+                j += 1
+        this_line_generated_text += this_line_text + "\n"
+        generated_text += this_line_generated_text
+        dialogues.pop(i)
+    generated_text += "".join(blanks_dialogues) + " \n"
+    return generated_text
+
+
+def __generate_lrc_timestamp(dt):
+    return "[" + str(dt.minute).zfill(2) + ":" + \
+        str(dt.second).zfill(2) + "." + \
+        str(int(dt.microsecond/10000)).zfill(2) + "]"
+
+
+def __convert_ass_effect_word_to_text(text):
+    if isinstance(text, list):
+        text2 = ""
+        for item2 in text:
+            text2 += item2.word
+        return text2
+    else:
+        return text
+
+
 def convert_to_normal_lrc(dialogues, space_end_timespan_ms):
     generated_text = ""
     dialogues.sort()
     for index, item in enumerate(dialogues):
-        lryic = "[" + str(item.start_time.minute).zfill(2) + ":" + \
-            str(item.start_time.second).zfill(2) + "." + \
-            str(int(item.start_time.microsecond/10000)).zfill(2) + "]"
+        lryic = __generate_lrc_timestamp(item.start_time)
         if isinstance(item.text, list):
             text = ""
             for item2 in item.text:
@@ -90,17 +138,13 @@ def convert_to_normal_lrc(dialogues, space_end_timespan_ms):
         else:
             lryic += item.text
         generated_text += lryic + "\n"
-        #append ending
+        # append ending
         if not index == len(dialogues) - 1:
             td = dialogues[index + 1].start_time - item.end_time
             if td.total_seconds() * 1000 > space_end_timespan_ms:
-                lryic = "[" + str(item.end_time.minute).zfill(2) + ":" + \
-                    str(item.end_time.second).zfill(2) + "." + \
-                    str(int(item.end_time.microsecond / 10000)).zfill(2) + "]"
+                lryic = __generate_lrc_timestamp(item.start_time)
                 generated_text += lryic + "\n"
         else:
-            lryic = "[" + str(item.end_time.minute).zfill(2) + ":" + \
-                    str(item.end_time.second).zfill(2) + "." + \
-                    str(int(item.end_time.microsecond / 10000)).zfill(2) + "]"
+            lryic = __generate_lrc_timestamp(item.start_time)
             generated_text += lryic + "\n"
     return generated_text
